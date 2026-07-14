7b73d2932cd9cde298925485a43a93864e979b87
𝙸𝚝𝚣・𝙼𝚒𝚕𝚌𝚒𝚘𝚂𝚂𝚀 <milanveresmaier@gmail.com>
2026-07-14 22:19:47 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  getSystemInfo: () => ipcRenderer.invoke('get-system-info'),
-  runTool: (toolName) => ipcRenderer.invoke('run-tool', toolName),
-  getDiskInfo: () => ipcRenderer.invoke('get-disk-info'),
-});
